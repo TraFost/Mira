4aac4a291801f851a77fd3a05ad5c1953c5b2dc6
Rahman Nurudin <rahwisdilfiqrak@gmail.com>
2025-07-27 22:15:04 +0700

    refactor: simplify database connection configuration by removing redundant client setup

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,41 +1,13 @@
-import dotenv from "dotenv";
 import { defineConfig } from "drizzle-kit";
-import { Client } from "pg";
-
-dotenv.config();
-
-const client = new Client({
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT_NO || "5432"),
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
-
-client
-  .connect()
-  .then(() => {
-    console.log("✅ Connected to PostgreSQL!");
-    return client.end();
-  })
-  .catch((err) => {
-    console.error("❌ Connection failed:", err);
-  });
 
 export default defineConfig({
   schema: "./app/db/schema/index.schema.ts",
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
-    host: process.env.DB_HOST!,
-    port: parseInt(process.env.DB_PORT_NO!),
-    user: process.env.DB_USERNAME!,
-    password: process.env.DB_PASSWORD!,
-    database: process.env.DB_NAME!,
-    ssl: false,
+    url:
+      process.env.DATABASE_URL ||
+      "postgresql://postgres:admin@localhost:5432/mira?sslmode=disable",
   },
   verbose: true,
   strict: true,
