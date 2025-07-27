babedd27a7c5293f9c4a85b16970a4098774d217
Rahman Nurudin <rahwisdilfiqrak@gmail.com>
2025-07-27 22:15:25 +0700

    refactor: update ESLint configuration and adjust path mappings in tsconfig

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	eslint: {
-		dirs: ["pages", "utils", "components", "services"],
-	},
+  eslint: {
+    dirs: ["pages", "utils", "components", "services"],
+  },
+  env: {
+    NEXT_PUBLIC_WC_PROJECT_ID: process.env.NEXT_PUBLIC_WC_PROJECT_ID,
+    DATABASE_URL: process.env.DATABASE_URL,
+  },
 };
 
 export default nextConfig;
