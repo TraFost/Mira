694cff615ebad52bf2132677f7878510542e6ab8
Samsul <113889106+samsulpanjul@users.noreply.github.com>
2025-07-27 21:19:59 +0700

    feat: enhance login page styling and layout

--- app/(main)/login/page.tsx
+++ app/(main)/login/page.tsx
@@ -2,14 +2,14 @@ const btn = ["💔 Blade Wallet", "🔥 WalletConnect"];
 
 export default function LoginPage() {
   return (
-    <div className="w-full h-screen flex justify-center items-center px-4">
-      <div className="flex flex-col gap-10 border-4 p-4 w-full md:w-lg lg:w-xl h-fit bg-[#DAF7DC]">
-        <h1 className="text-4xl font-bold">Login with</h1>
-        <div className="flex flex-col gap-4 mb-6">
+    <div className="w-full h-screen flex justify-center items-center px-4 bg-background text-foreground">
+      <div className="flex flex-col gap-10 p-8 w-full max-w-md rounded-2xl backdrop-blur-md bg-foreground/5 border border-foreground/20 shadow-xl">
+        <h1 className="text-4xl font-bold text-center">Login with</h1>
+        <div className="flex flex-col gap-4">
           {btn.map((b) => (
             <button
               key={b}
-              className="border-2 font-bold bg-[#8390FA] text-xl cursor-pointer py-3"
+              className="cursor-pointer text-lg font-semibold py-3 rounded-full bg-gradient-to-r from-secondary via-foreground to-gradient text-background hover:opacity-90 transition"
             >
               {b}
             </button>
